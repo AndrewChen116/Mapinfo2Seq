75b12853f093d5be1a05bf57b2b9ecc5d814834b
Andrew Chen <50938805+AndrewChen116@users.noreply.github.com>
2023-10-03 17:06:38 +0800

    solve the hgsid issue
    add program to extract hgsid
    simulate user behavior to avoid the server blocking

--- FUN_seqCrawler.R
+++ FUN_seqCrawler.R
@@ -1,6 +1,9 @@
 rm(list = ls(), envir = globalenv())
 genome_seq_crawler <- function(df, applyParallel=F, core=4){
   require(parallel)
+  require(tidyverse)
+  require(XML)
+  require(RCurl)
   
   id.set <- df$id %>% as.character()
   db.set <- df$species %>% as.character()
@@ -8,26 +11,42 @@ genome_seq_crawler <- function(df, applyParallel=F, core=4){
   start.set <- df$start %>% as.numeric()
   end.set <- df$end %>% as.numeric()
   strand.set <- df$strand %>% as.character()
-  
+  hgsid.set <- c()
+
+  cat("hgsid generating...\n")
+  for(i in 1:core){
+    hgsid.set[i] <- paste0(
+      "https://genome.ucsc.edu/cgi-bin/hgc?",
+      "hgsid=",i,"&",
+      "o=30514214&g=getDna&i=mixed&c=chr3&l=30514214&",
+      "r=30514615&db=hg38"
+    ) %>% getURL %>% htmlParse %>% 
+      xpathApply("/html/body/div[1]/div/div/div[2]/div/div/ul/li[3]/a",xmlGetAttr,"href") %>% 
+      str_extract('(?<=hgsid=).*')
+  }
+  print(hgsid.set)
+  hgsid.set <- hgsid.set %>% rep((nrow(df)/core)+1)
+
   Apply_FUN <- function(n){
     require(tidyverse)
     require(XML)
+    require(RCurl)
     
     id <- id.set[n]
     db <- db.set[n]
     chr <- chr.set[n]
     start <- start.set[n]
     end <- end.set[n]
-    
+    hgsid <- hgsid.set[n]
     if(as.character(strand.set[n]) == "-"){
       strand <- "hgSeq.revComp=on&"
     }else{
       strand <- ""
     }
-    
+
     request_url <-  paste0(
       "http://genome.ucsc.edu/cgi-bin/hgc?",
-      "hgsid=1715286470_Lm1S0iOuLAqMkwq6b9ATFbHihMHo&g=htcGetDna2&",
+      "hgsid=",hgsid,"&g=htcGetDna2&",
       "table=&i=mixed&o=",(start-1),"&l=",(start-1),"&r=",end,
       "&getDnaPos=chr",chr,":",start,"-",end,
       "&db=",db, "&hgSeq.cdsExon=1&&hgSeq.padding5=0",
@@ -35,7 +54,6 @@ genome_seq_crawler <- function(df, applyParallel=F, core=4){
       "boolshad.hgSeq.maskRepeats=0&hgSeq.repMasking=lower&",
       strand,"boolshad.hgSeq.revComp=0&submit=get+DNA"
     ) %>% str_remove_all("\n") %>% str_remove_all(" ")
-    
     
     html_content <- htmlParse(request_url) %>% 
       xpathSApply('//pre') %>% `[[`(1) %>% `[[`(1) %>% xmlValue %>% 
@@ -53,6 +71,7 @@ genome_seq_crawler <- function(df, applyParallel=F, core=4){
     applied.env[["start.set"]] = start.set
     applied.env[["end.set"]] = end.set
     applied.env[["strand.set"]] = strand.set
+    applied.env[["hgsid.set"]] = hgsid.set
     
     cl <- makeCluster(core)
     clusterExport(cl, "db.set", envir = applied.env)
@@ -61,6 +80,7 @@ genome_seq_crawler <- function(df, applyParallel=F, core=4){
     clusterExport(cl, "start.set", envir = applied.env)
     clusterExport(cl, "end.set", envir = applied.env)
     clusterExport(cl, "strand.set", envir = applied.env)
+    clusterExport(cl, "hgsid.set", envir = applied.env)
     
     content.lt <- parLapply(
       cl, c(1:nrow(df)), Apply_FUN
